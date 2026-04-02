4fcd10d2d558a9692690d8f858a3d1eb3b2f8b0d
meats88 <pcx883mi@gmail.com>
2026-04-01 17:49:17 -0700

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
+    int value;
+    int maxval;
     // TODO
-
+cout << "give ten" << endl;
+cin >> maxval;
+for (int i = 0; i < 9; i++){
+cin >> value;
+if (value > maxval){
+    maxval = value;
+}
+}
+cout << maxval << endl;
     return 0;
 }
